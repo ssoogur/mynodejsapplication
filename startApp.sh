d1ea92f491bd1c2efa5cc262941c79bb170067c7
ssoogur <55739704+ssoogur@users.noreply.github.com>
2021-11-26 22:53:48 -0500

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.cohobyukovto.ap-south-1.rds.amazonaws.com
-export DB_PRD_USER=dellstoreaws
-export DB_PRD_PASS=dellstoreaws
+export DB_PRD_HOST=testdb.cgqljkneiaev.us-east-2.rds.amazonaws.com
+export DB_PRD_USER=postgres
+export DB_PRD_PASS=y38r3nd2020
 export NODE_HOST=localhost
 export NODE_PORT=8080
 node /myapp/index.js&
