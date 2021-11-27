6af57d7cc7b8bb027bb2756bc475c8c7c3536390
ssoogur <55739704+ssoogur@users.noreply.github.com>
2021-11-27 18:02:42 -0500

    Update startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -5,5 +5,6 @@ export DB_PRD_USER=postgres
 export DB_PRD_PASS=y38r3nd2020
 export NODE_HOST=localhost
 export NODE_PORT=8080
-node /myapp/index.js&
+node /myapp/index.js >/dev/null 2>&1 &
+
 exit 0
